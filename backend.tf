839077ffc932d7aa9d0c015a1136bf06a2d276c2
LifebookAI <founder@uselifebook.ai>
2026-05-02 22:19:12 -0400

    Make Terraform backend compatible with CI credentials

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket       = "lifebookai-tfstate-354630286254-us-east-1"
     key          = "aws-serverless-file-pipeline-terraform-lab/dev/terraform.tfstate"
     region       = "us-east-1"
-    profile      = "lifebook-sso"
     encrypt      = true
     use_lockfile = true
   }
