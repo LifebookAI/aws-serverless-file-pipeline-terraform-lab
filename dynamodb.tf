db9827905217c64dee64aec0e43c8c011394eb66
LifebookAI <founder@uselifebook.ai>
2026-05-02 20:28:06 -0400

    Add Terraform-managed DynamoDB metadata table

---
+++ dynamodb.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "file_metadata" {
+  name         = "${local.name_prefix}-file-metadata"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "file_id"
+
+  attribute {
+    name = "file_id"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-file-metadata"
+  }
+}
